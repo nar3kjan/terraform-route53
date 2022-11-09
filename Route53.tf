44cc5f0c5043a0112e9e03aff0fa3f9e82637cfb
Narek <zzz@zzz.zzz>
2022-11-09 16:09:01 +0400

    www

--- Route53.tf
+++ Route53.tf
@@ -61,43 +61,10 @@ resource "aws_acm_certificate_validation" "validation_www" {
 }
 
 
-resource "aws_acm_certificate" "www" {
-  domain_name       = "www.nar3kjan.link"
-  validation_method = "DNS"
-}
-
-
-
-resource "aws_route53_record" "my_www_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.www.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.my_zone.zone_id
-}
-
-
-
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.www.arn
-  validation_record_fqdns = [for record in aws_route53_record.my_www_record : record.fqdn]
-}
-
-
-
 
 resource "aws_route53_record" "www_elb" {
   zone_id = data.aws_route53_zone.my_zone.id
-  name    = "www"
+  name    = "www.nar3kjan.link"
   type    = "A"
 
   alias {
