132a838f833593c110ecf9e71ac591cf89a3427b
Narek <zzz@zzz.zzz>
2022-11-09 16:13:08 +0400

    www

--- Route53.tf
+++ Route53.tf
@@ -64,7 +64,7 @@ resource "aws_acm_certificate_validation" "validation_www" {
 
 resource "aws_route53_record" "www_elb" {
   zone_id = data.aws_route53_zone.my_zone.id
-  name    = "www.nar3kjan.link"
+  name    = "nar3kjan.link"
   type    = "A"
 
   alias {
